e1d3b8d80cd0e329a1eaa80cdbb9af56568a0264
langley2119 <42552653+langley2119@users.noreply.github.com>
2019-04-27 17:42:53 -0500

    more code for ir sensor

---
+++ PIRSensorcode.ino
@@ -0,0 +1,66 @@
+//////////////////////////
+// Hardware Definitions //
+//////////////////////////
+#define PIR_AOUT A0  // PIR analog output on A0
+#define PIR_DOUT 2   // PIR digital output on D2
+#define LED_PIN  13  // LED to illuminate on motion
+
+#define PRINT_TIME 100 // Rate of serial printouts
+unsigned long lastPrint = 0; // Keep track of last serial out
+
+void setup() 
+{
+  Serial.begin(115200);  // Serial is used to view Analog out
+  // Analog and digital pins should both be set as inputs:
+  pinMode(PIR_AOUT, INPUT);
+  pinMode(PIR_DOUT, INPUT);
+
+  // Configure the motion indicator LED pin as an output
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW); // Turn the LED off
+}
+
+void loop() 
+{
+  // Read OUT pin, and set onboard LED to mirror output
+  readDigitalValue(); 
+
+  // Read A pin, print that value to serial port:
+  printAnalogValue();
+}
+
+void readDigitalValue()
+{
+  // The OpenPIR's digital output is active high
+  int motionStatus = digitalRead(PIR_DOUT);
+
+  // If motion is detected, turn the onboard LED on:
+  if (motionStatus == HIGH)
+    digitalWrite(LED_PIN, HIGH);
+  else // Otherwise turn the LED off:
+    digitalWrite(LED_PIN, LOW);
+}
+
+void printAnalogValue()
+{
+  if ( (lastPrint + PRINT_TIME) < millis() )
+  {
+    lastPrint = millis();
+    // Read in analog value:
+    unsigned int analogPIR = analogRead(PIR_AOUT);
+    // Convert 10-bit analog value to a voltage
+    // (Assume high voltage is 5.0V.)
+    float voltage = (float) analogPIR / 1024.0 * 5.0;
+    // Print the reading from the digital pin.
+    // Mutliply by 5 to maintain scale with AOUT. 
+    Serial.print(5 * digitalRead(PIR_DOUT));
+    Serial.print(',');    // Print a comma
+    Serial.print(2.5);    // Print the upper limit
+    Serial.print(',');    // Print a comma
+    Serial.print(1.7);    // Print the lower limit
+    Serial.print(',');    // Print a comma
+    Serial.print(voltage); // Print voltage
+    Serial.println();
+  }
+}
+
