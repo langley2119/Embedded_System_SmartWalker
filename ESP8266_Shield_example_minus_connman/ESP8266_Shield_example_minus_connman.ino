e00162de7a01e8d2ed2cb531a6737b33909f67d4
Jackson Farley <FarleyJ@ECS.BAYLOR.EDU>
2019-04-27 16:02:19 -0500

    added the WiFi base code and libraries

---
+++ ESP8266_Shield_example_minus_connman/ESP8266_Shield_example_minus_connman.ino
@@ -0,0 +1,117 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This example shows how to use ESP8266 Shield (with AT commands)
+  to connect your project to Blynk.
+
+  WARNING!
+    It's very tricky to get it working. Please read this article:
+    http://help.blynk.cc/hardware-and-libraries/arduino/esp8266-with-at-firmware
+
+  Change WiFi ssid, pass, and Blynk auth token to run :)
+  Feel free to apply it to any other example. It's simple!
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+//#include <queue.h>
+//#include <ESP8266WiFi.h>
+
+#include <ESP8266_Lib.h>
+#include <BlynkSimpleShieldEsp8266.h>
+
+// for the wifi manager
+
+
+//#include <WiFiManager.h> // Wifi configuration manager used thanks to tazpu
+//#include <DNSServer.h>
+//#include <ESP8266WebServer.h>
+
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "a2f791ce584c4b91a59bdc40e6e9b4d9";
+
+// including the timer necessary instead of delays (so that blink can still run compatibly) 
+BlynkTimer timer; 
+
+// adding the wifi Manager
+//WifiManager wifiManager; 
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "iPhone";
+char pass[] = "morecoffee";
+
+// Hardware Serial on Mega, Leonardo, Micro...
+#define EspSerial Serial1
+
+// or Software Serial on Uno, Nano...
+//#include <SoftwareSerial.h>
+//SoftwareSerial EspSerial(10, 11); // RX, TX
+
+// Your ESP8266 baud rate:
+#define ESP8266_BAUD 9600
+
+ESP8266 wifi(&EspSerial);
+
+void myTimerEvent()
+{ 
+  Blynk.virtualWrite(V5, (millis()/1000) % 10); 
+}
+
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  delay(10);
+
+  // Set ESP8266 baud rate
+  EspSerial.begin(ESP8266_BAUD);
+  delay(10);
+
+  // setting up function calls via the timer 
+  // first argument is the duration between events in milliseconds 
+  // up to 16 timers per timer object. 
+  // max data sending rate of 10 values per second
+  timer.setInterval(1000L,myTimerEvent); 
+
+  // WIFI Manager Attempts 
+  // one-time thing: 
+  //wifiManager.resetSettings(); 
+  // setting up the auto connnect using WiFiManager Library
+  //wifiManager.setConfigPortalTimeout(180); // waits 3 minutes and then will shut off. 
+  //wifiManager.autoConnect("RemindME Walker","remindme"); 
+  
+  Blynk.begin(auth, wifi, ssid, pass);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); 
+}
+
+
+
+
---
+++ libraries/SparkFun_RFD77402_Arduino_Library-master/README.md
@@ -0,0 +1,42 @@
+SparkFun RFDigital RFD77402 Interface Library
+===========================================================
+
+![SparkFun Distance Sensor - RFD77402](https://cdn.sparkfun.com/r/500-500/assets/parts/1/2/6/5/9/14539-SparkFun_Distance_Sensor_Breakout_-_RFD77402__Qwiic_-01.jpg)
+
+[*Distance Sensor-RFD77402 (SEN-14539)*](https://www.sparkfun.com/products/14539)
+
+The Qwiic Distance Sensor uses the RFD77402 sensor utilizing VCSEL or vertical-cavity surface-emitting laser and a 3 pixel camera to measure the amount of time to a target. This gives you amazing millimeter prescision with low-cost and low power.
+
+Library written by Nathan Seidle ([SparkFun](http://www.sparkfun.com)).
+
+Repository Contents
+-------------------
+
+* **/examples** - Example sketches for the library (.ino). Run these from the Arduino IDE. 
+* **/src** - Source files for the library (.cpp, .h).
+* **keywords.txt** - Keywords from this library that will be highlighted in the Arduino IDE. 
+* **library.properties** - General library properties for the Arduino package manager. 
+
+Documentation
+--------------
+
+* **[Installing an Arduino Library Guide](https://learn.sparkfun.com/tutorials/installing-an-arduino-library)** - Basic information on how to install an Arduino library.
+* **[Product Repository](https://github.com/sparkfun/Qwiic_Distance_RFD77402)** - Main repository (including hardware files) for the Qwiic Distance RFD77402 breakout board.
+
+Product Versions
+----------------
+* [SEN-14539](https://www.sparkfun.com/products/14539)
+* [SPX-14343](https://www.sparkfun.com/products/14343)
+
+License Information
+-------------------
+
+This product is _**open source**_! 
+
+Various bits of the code have different licenses applied. Anything SparkFun wrote is beerware; if you see me (or any other SparkFun employee) at the local, and you've found our code helpful, please buy us a round! Anything Maxim wrote has its own license. Anything that was co-writing with Peter Jansen is BSD.
+
+Please use, reuse, and modify these files as you see fit. Please maintain attribution to SparkFun Electronics and release anything derivative under the same license.
+
+Distributed as-is; no warranty is given.
+
+- Your friends at SparkFun.
